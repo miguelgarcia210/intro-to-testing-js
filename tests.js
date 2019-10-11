b08e2a2580cf16f41bf85ab036987f69d08cef9b
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-11 17:14:09 -0500

    add the fourth test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -24,4 +24,7 @@ describe('sayHello', function() {
     it('should return "Hello, Jane!"', function () {
         expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
+    it('should return "Hello, Alex!"', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
 });
